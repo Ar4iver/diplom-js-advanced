0b3d12c65be287828e5f9f7031c1bae9afdf9837
Igor <qwertyhock@gmail.com>
2023-06-14 20:07:00 +0300

    congig

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,54 +1,67 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const webpack = require('webpack')
 const path = require('path')
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
+module.exports = (env) => {
+  const mode = env.mode || 'development'
+  const PORT = env.port || 3000
+  const isDev = mode === 'development'
+
+  console.log(mode)
+
+  return {
+    mode,
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
     },
-    open: true,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react'],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+      }),
+      new webpack.ProgressPlugin(),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react'],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.jsx', '.js'],
-    alias: {
-      '@mui/system/jsx-runtime': '@mui/system/jsx-dev-runtime', // Подключаем jsx-runtime из @mui/system
+        {
+          test: /\.css$/i,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+      ],
     },
-  },
+    resolve: {
+      extensions: ['.jsx', '.js'],
+    },
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev
+      ? {
+          open: true,
+          historyApiFallback: true,
+        }
+      : undefined,
+  }
 }
